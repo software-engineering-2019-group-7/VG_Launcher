8669c4f0952eaa17d3a362d48713c294c8af4683
Matthew <matthew.sutton09@gmail.com>
2019-06-03 12:13:11 -0400

    Progress

---
+++ VG_Launcher/GameScreen.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace VG_Launcher
+{
+    /// <summary>
+    /// Interaction logic for GameScreen.xaml
+    /// </summary>
+    public partial class GameScreen : Window
+    {
+        string senderName;
+
+
+        public GameScreen()
+        {
+            InitializeComponent();
+           
+        }
+
+    }
+}
--- VG_Launcher/MainWindow.xaml.cs
+++ VG_Launcher/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
@@ -20,47 +23,37 @@ namespace VG_Launcher
 
         void CreateButtons()
         {
-                Button btn = new Button();
+            Button btn = new Button();
 
-                ///this chunk will let us set the buttons to whatever we wanted them to look like.
-                ///Picture backgrounds included, but they need to be downloaded as of right now. Will look into this.
+            ///this chunk will let us set the buttons to whatever we wanted them to look like.
+            ///Picture backgrounds included, but they need to be downloaded as of right now. Will look into this.
+            btn.Name = "button" + gameWrapPanel.Children.Count;
+            btn.Content = "Path of Exile"; //replace this with the name of the game recieved
+            btn.Width = 360;
+            btn.Height = 160;
+            btn.Margin = new Thickness(10);
+            btn.HorizontalContentAlignment = HorizontalAlignment.Center;
+            btn.VerticalContentAlignment = VerticalAlignment.Bottom;
+            btn.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4CFFFFFF"));
+            btn.FontSize = 48;
+            btn.FontWeight = FontWeights.SemiBold;
 
-                btn.Name = "button" + gameWrapPanel.Children.Count;
-                btn.Content = "Path of Exile"; //replace this with the name of the game recieved
-                btn.Width = 360;
-                btn.Height = 160;
-                btn.Margin = new Thickness(10);
-                btn.HorizontalContentAlignment = HorizontalAlignment.Center;
-                btn.VerticalContentAlignment = VerticalAlignment.Bottom;
-                btn.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4CFFFFFF"));
-                btn.FontSize = 48;
-                btn.FontWeight = FontWeights.SemiBold;
+            ///This section deals with the background picture. We will have to change this for sure if we are getting them from the internet. 
+            ///I suppose we will have to download the images regardless, so maybe we will have the actual images at this point? 
+            ///Steam and others load a default background and replace it when they have the actual image (maybe do this on a different thread?)
+            Uri resourceUri = new Uri("Resources/header.jpg", UriKind.Relative);
+            StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+            BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+            var brush = new ImageBrush();
+            brush.ImageSource = temp;
+            btn.Background = brush;
 
-                ///This section deals with the background picture. We will have to change this for sure if we are getting them from the internet. 
-                ///I suppose we will have to download the images regardless, so maybe we will have the actual images at his point. 
-                ///Steam and others load a default background and replace it when they have the actual image (maybe do this on a different thread?)
-                Uri resourceUri = new Uri("Resources/header.jpg", UriKind.Relative);
-                StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
-                BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
-                var brush = new ImageBrush();
-                brush.ImageSource = temp;
-                btn.Background = brush;
+            //This lets us click the button. All of the buttons will share a function called Button_Click so we will have to be creative.
+            //Theres no way we can create a method for each new button, at least not that I know of. 
+            btn.Click += Button_Click;
 
-                //This lets us click the button. All of the buttons will share a function called Button_Click so we will have to be creative.
-                //Theres no way we can create a method for each new button, at least not that I know of. 
-                btn.Click += Button_Click;
+            gameWrapPanel.Children.Add(btn);
 
-
-                // A dropshadow would look nice on the added buttons but I cant seem to make it work. I will look into this
-                //
-                //btn.Effect = new DropShadowEffect()
-                //{
-                //    BlurRadius = 10,
-                //    ShadowDepth = 5
-                //};
-
-                gameWrapPanel.Children.Add(btn);
-            
         }
 
         private void ServiceLoader_Click(object sender, RoutedEventArgs e)
@@ -77,8 +70,19 @@ namespace VG_Launcher
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button; //lets us edit the button that sent the function call
-            btn.Content = "Clicked";
-            MessageBox.Show(btn.Name + " Clicked");
+            GameScreen gs = new GameScreen();
+            gs.Title = btn.Content.ToString();
+            this.Effect = new BlurEffect();
+            gs.WindowStartupLocation = WindowStartupLocation.Manual;
+            gs.Left = PointToScreen(Mouse.GetPosition(null)).X-270;  //this tells the game screen where to open. Maybe with some slick math we can set it to open directly under the 
+            gs.Top = PointToScreen(Mouse.GetPosition(null)).Y+35;   //button pressed. This is close enough as of right now.
+
+            //I really wanted to not have this as a dialog, but I was having trouble 
+            //with dynamically closing and reopening windows as I was clicking on the game buttons
+            //I had it working, but if I clicked the same game button as the one I had just previously 
+            //clicked the program would crash.
+            gs.ShowDialog(); 
+            this.Effect = null;
         }
     }
 }
