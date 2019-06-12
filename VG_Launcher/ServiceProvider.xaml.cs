4d18185e5dfeaf8a408b49acb72a706d71fa5671
Ispil <ispil@outlook.com>
2019-06-12 12:57:02 -0400

    Added the GetSteamGameDirectoryList function, which returns all the directory paths of the user's games.

--- VG_Launcher/ServiceProvider.xaml.cs
+++ VG_Launcher/ServiceProvider.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -26,6 +29,29 @@ namespace VG_Launcher
             }
             //SomeFunctionThatWillTakeInTheList();
             this.Close();
+        }
+        private string[] GetSteamGameDirectoryList()
+        {
+            string steamInstallPath = "";
+            RegistryKey localMachineRegistry = Environment.Is64BitOperatingSystem ? RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64) : RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+            RegistryKey steamRegistryKey = localMachineRegistry.OpenSubKey(@"SOFTWARE\Valve\Steam");
+            if (steamRegistryKey == null)
+            {
+                localMachineRegistry.Close();
+                localMachineRegistry = Environment.Is64BitOperatingSystem ? RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32) : RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                steamRegistryKey = localMachineRegistry.OpenSubKey(@"SOFTWARE\Valve\Steam");
+            }
+            object registryEntry = steamRegistryKey.GetValue("InstallPath");
+            if (registryEntry != null)
+                steamInstallPath = (string)registryEntry;
+            localMachineRegistry.Close();
+            steamRegistryKey.Close();
+            string steamLibraryPath = Path.Combine(steamInstallPath, @"steamapps");
+            // For the sake of simplicity (since it can only be done manually these days), we will assume that the user only has one Steam library folder, and that it is not external. This means no reading from libraryfolders.vdf
+            steamLibraryPath = Path.Combine(steamLibraryPath, @"common");
+            if (Directory.Exists(steamLibraryPath))
+                return Directory.GetDirectories(steamLibraryPath);
+            return null;
         }
     }
 }
